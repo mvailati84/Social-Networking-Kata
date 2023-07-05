2a5540685bcaab2f05c212257c6676140f68e217
mvailati <mvailati@bmc.com>
2023-07-05 18:10:22 +0200

    Few refactoring

--- src/main/java/com/example/socialnet/model/UserMessage.java
+++ src/main/java/com/example/socialnet/model/UserMessage.java
@@ -11,7 +11,8 @@ import lombok.*;
 @NoArgsConstructor(force = true)
 public class UserMessage {
     private static final double A_SECOND = 1000;
-    private static final double AN_HOUR = A_SECOND * 60 * 60;
+    private static final double A_MINUTE = A_SECOND * 60;
+    private static final double AN_HOUR = A_MINUTE * 60;
     private static final double A_DAY = AN_HOUR * 24;
     @Id
     @GeneratedValue
@@ -29,14 +30,19 @@ public class UserMessage {
 
         if (elapsed < A_SECOND){
             return "(less than 1 second ago)";
-        }else if (elapsed < AN_HOUR){
-            double hoursElapsed = elapsed / AN_HOUR;
-            return "(" + hoursElapsed + " hour" + plural(hoursElapsed) + " ago)";
+        }else if (elapsed < A_MINUTE){
+            return printElapsedTime(elapsed, A_MINUTE, "minute");
+        } else if (elapsed < AN_HOUR) {
+            return printElapsedTime(elapsed, AN_HOUR, "hour");
         }
-
-        double daysElapsed = elapsed / A_DAY;
-        return "(" + daysElapsed + " day" + plural(daysElapsed) + " ago)";
+        return printElapsedTime(elapsed, A_DAY, "day");
     }
+
+    private String printElapsedTime(long elapsed, double timeUnit, String timeLabel) {
+        double minutesElapsed = elapsed / timeUnit;
+        return "(" + minutesElapsed + " " + timeLabel + plural(minutesElapsed) + " ago)";
+    }
+
 
     private String plural(double num) {
         return num > 1 ? "s" : "";
