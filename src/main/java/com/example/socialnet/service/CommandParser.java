58c07b61341df58aad8005baf53481410ebeb0de
mvailati <mvailati@bmc.com>
2023-07-06 11:36:27 +0200

    Implement command parser

--- src/main/java/com/example/socialnet/service/CommandParser.java
+++ src/main/java/com/example/socialnet/service/CommandParser.java
@@ -6,16 +6,65 @@ import org.springframework.stereotype.Service;
 @Service
 public class CommandParser {
 
-
     public Command parse(String line) {
-        String input = line.trim();
+        String[] message = line.trim().split(" ", 3);
 
-        if ("exit".equalsIgnoreCase(input)){
+        if (isAPostingMessage(message)) {
+            return Command.builder()
+                    .type(Command.CommandType.POSTING)
+                    .username(message[0])
+                    .message(message[2])
+                    .build();
+        }
+
+        if (isAReadingMessage(message)) {
+            return Command.builder()
+                    .type(Command.CommandType.READING)
+                    .username(message[0])
+                    .build();
+        }
+
+        if (isAFollowingMessage(message)) {
+            return Command.builder()
+                    .type(Command.CommandType.FOLLOWING)
+                    .username(message[0])
+                    .message(message[2])
+                    .build();
+        }
+
+        if (isAWallMessage(message)) {
+            return Command.builder()
+                    .type(Command.CommandType.WALL)
+                    .username(message[0])
+                    .build();
+        }
+
+        if (isAnExistMessage(message)){
             return Command.builder()
                     .type(Command.CommandType.EXIT)
                     .build();
         }
 
         return null;
+    }
+
+    private boolean isAWallMessage(String[] message) {
+        return message.length == 2 && "wall".equalsIgnoreCase(message[1]);
+    }
+
+    private boolean isAFollowingMessage(String[] message) {
+        return message.length == 3 && "follows".equalsIgnoreCase(message[1]);
+    }
+
+    private boolean isAReadingMessage(String[] message) {
+        return message.length == 1 && !"exit".equalsIgnoreCase(message[0]);
+    }
+
+    private boolean isAnExistMessage(String[] message) {
+        return message.length == 1 && "exit".equalsIgnoreCase(message[0]);
+    }
+
+    private boolean isAPostingMessage(String[] message) {
+        return message.length == 3 && "->".equalsIgnoreCase(message[1]);
     }
 }
--- src/test/java/com/example/socialnet/service/CommandParserTest.java
+++ src/test/java/com/example/socialnet/service/CommandParserTest.java
@@ -1,11 +1,12 @@
 package com.example.socialnet.service;
 
 import com.example.socialnet.model.Command;
-import com.example.socialnet.model.UserMessage;
+import com.example.socialnet.model.Command.CommandType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class CommandParserTest {
 
@@ -19,8 +20,41 @@ class CommandParserTest {
     @Test
     void whenLineIsExit_thenCommandIsExit() {
         Command command = parser.parse("exit");
-
         assertTrue(command.isExit());
+    }
+
+    @Test
+    void whenPostingMessage_thenCommandIsPosting() {
+        Command command = parser.parse("Alice -> I love the weather today");
+
+        assertEquals(CommandType.POSTING, command.getType());
+        assertEquals("Alice", command.getUsername());
+        assertEquals("I love the weather today", command.getMessage());
+
+    }
+    @Test
+    void whenReadingMessage_thenCommandIsReading() {
+        Command command = parser.parse("Alice");
+
+        assertEquals(CommandType.READING, command.getType());
+        assertEquals("Alice", command.getUsername());
+    }
+
+    @Test
+    void whenWallMessage_thenCommandIsWall() {
+        Command command = parser.parse("Alice wall");
+
+        assertEquals(CommandType.WALL, command.getType());
+        assertEquals("Alice", command.getUsername());
+    }
+
+    @Test
+    void whenFollowingMessage_thenCommandIsFollowing() {
+        Command command = parser.parse("Alice follows Bob");
+
+        assertEquals(CommandType.FOLLOWING, command.getType());
+        assertEquals("Alice", command.getUsername());
+        assertEquals("Bob", command.getMessage());
     }
 
 }
